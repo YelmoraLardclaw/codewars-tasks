60dde9eecc755ed237ad7706be96bad992255746
Yelmora <yelmora3008@gmail.com>
2020-02-01 16:30:45 +0400

    Task 4

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -48,3 +48,12 @@ def find_nb(m):
 # The second value in the first integer array is 0, since the bus is empty in the first bus stop.
 def number(bus_stops):
     return sum([stop[0] for stop in bus_stops]) - sum([stop[1] for stop in bus_stops])
+
+
+## 4 ##
+# Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built
+# with the sides of given length and false in any other case.
+#
+# (In this case, all triangles must have surface greater than 0 to be accepted).
+def is_triangle(a, b, c):
+    return (a + b) > c and (a + c) > b and (b + c) > a
